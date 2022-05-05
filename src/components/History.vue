edc6430c53487d5e6292a11b5f17bafdaae4b157
Razor7w <seba.carroza@gmail.com>
2022-05-05 08:01:21 -0400

    [Feat] - Se añade vista "History" - Se agrega buscador en la tabla

---
+++ src/components/History.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card flat>
+    <v-card-title>
+      Historial de consultas
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="mindicadorInstance.history"
+      :items-per-page="5"
+      class="elevation-1"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import MindicadorModule from '@/store/modules/mindicador'
+
+@Component
+export default class History extends Vue {
+  mindicadorInstance = getModule(MindicadorModule, this.$store)
+  search = ''
+  headers = [
+    {
+      text: 'Fecha y hora de la actividad',
+      align: 'start',
+      sortable: false,
+      value: 'date'
+    },
+    { text: 'Usuario de la solicitud', value: 'user' },
+    { text: 'Monto de origen', value: 'amount' },
+    { text: 'Fecha conversión', value: 'dateSelected' },
+    { text: 'Valor de la moneda', value: 'currencyValue' },
+    { text: 'Monto de la conversión', value: 'conversionAmount' }
+  ]
+}
+</script>
